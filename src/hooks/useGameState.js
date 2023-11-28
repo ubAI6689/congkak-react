7d822dfcc75ca92bb460d913e83599ad702be4d2
ubAI6689 <ub.ai6689@gmail.com>
2023-11-29 01:17:02 +0800

    Factorized gameState variables.

--- src/hooks/useGameState.js
+++ src/hooks/useGameState.js
@@ -1,6 +1,9 @@
 import { useState, useCallback } from 'react';
 import config from '../config/config';
+<<<<<<< HEAD
 import gamePhaseConfig from '../config/gamePhaseConfig';
+=======
+>>>>>>> Factorized gameState variables.
 
 const { 
   INIT_SEEDS_COUNT,
@@ -13,6 +16,7 @@ const {
   MAX_INDEX_LOWER,
 } = config;
 
+<<<<<<< HEAD
 const {
   STARTING_PHASE,
   SIMULTANEOUS_SELECT,
@@ -23,12 +27,18 @@ const {
   TURN_BASED_SELECT
 } = gamePhaseConfig
 
+=======
+>>>>>>> Factorized gameState variables.
 const startIndexUpper = Math.round((MIN_INDEX_UPPER + MAX_INDEX_UPPER) / 2);
 const startIndexLower = Math.round((MIN_INDEX_LOWER + MAX_INDEX_LOWER) / 2);
 
 export const useGameState = () => {
   const [seeds, setSeeds] = useState(new Array(HOLE_NUMBERS).fill(INIT_SEEDS_COUNT)); // 14 holes excluding houses
+<<<<<<< HEAD
   const [gamePhase, setGamePhase] = useState(STARTING_PHASE);
+=======
+  const [gamePhase, setGamePhase] = useState('STARTING_PHASE'); // or 'TURN_BASED'
+>>>>>>> Factorized gameState variables.
   const [isStartButtonPressed, setIsStartButtonPressed] = useState(false);
   const [startingPositionUpper, setStartingPositionUpper] = useState(null);
   const [startingPositionLower, setStartingPositionLower] = useState(null);
@@ -70,9 +80,15 @@ export const useGameState = () => {
     setCurrentTurn(prevTurn => prevTurn === PLAYER_UPPER ? PLAYER_LOWER : PLAYER_UPPER);
   }, []);
 
+<<<<<<< HEAD
   // handle the logic for both START and RESUME button
   const startButtonPressed = useCallback((handleWrongSelection, setShakeCursor, simultaneousSowing) => {
     if (gamePhase === STARTING_PHASE) {
+=======
+  const startButtonPressed = useCallback((handleWrongSelection, setShakeCursor, simultaneousSowing) => {
+    // handle the logic for both START and RESUME button
+    if (gamePhase === 'STARTING_PHASE' || gamePhase === 'SIMULTANEOUS_SELECT') {
+>>>>>>> Factorized gameState variables.
       if (startingPositionUpper === null || seeds[startingPositionUpper] === 0) {
         console.log("startingPosUpper: ", startingPositionUpper);
         console.log("Please select starting position for Player Upper")
@@ -85,6 +101,7 @@ export const useGameState = () => {
         setIsStartButtonPressed(true);
         simultaneousSowing(startingPositionUpper, startingPositionLower);
       }
+<<<<<<< HEAD
     }
     else if (gamePhase === SIMULTANEOUS_SELECT) {
       if (startingPositionUpper === null || seeds[startingPositionUpper] === 0 || (startingPositionLower !== null && MAX_INDEX_UPPER - startingPositionUpper === MAX_INDEX_LOWER - startingPositionLower)) {
@@ -102,6 +119,10 @@ export const useGameState = () => {
       }
     // resume button logic
     } else if (gamePhase === SIMULTANEOUS_SELECT_UPPER) {
+=======
+    // resume button logic
+    } else if (gamePhase === 'SIMULTANEOUS_SELECT_UPPER') {
+>>>>>>> Factorized gameState variables.
       if (startingPositionUpper === null || seeds[startingPositionUpper] === 0 || startingPositionUpper === currentHoleIndexLower) {
         console.log("Please select starting position for Player Upper");
         handleWrongSelection(setShakeCursor);
@@ -109,7 +130,11 @@ export const useGameState = () => {
         setIsStartButtonPressed(true);
         simultaneousSowing(startingPositionUpper, null);
       }
+<<<<<<< HEAD
     } else if (gamePhase === SIMULTANEOUS_SELECT_LOWER) {
+=======
+    } else if (gamePhase === 'SIMULTANEOUS_SELECT_LOWER') {
+>>>>>>> Factorized gameState variables.
       if (startingPositionLower === null || seeds[startingPositionLower] === 0 || startingPositionLower === currentHoleIndexUpper) {
         console.log("Please select starting position for Player Lower");
         handleWrongSelection(setShakeCursor);
@@ -120,6 +145,7 @@ export const useGameState = () => {
     }
   },[gamePhase, startingPositionUpper, startingPositionLower, seeds, currentHoleIndexLower, currentHoleIndexUpper]);
 
+<<<<<<< HEAD
   const handleSButtonPress = useCallback(async (index, updateCursorPositionUpper, updateCursorPositionLower, holeRefs, topHouseRef, lowHouseRef, verticalPosUpper, verticalPosLower, turnBasedSowing, setShakeCursor, handleWrongSelection) => {
     if (!isSowingUpper) {
         // The logic that mimics the 'S' key press
@@ -140,12 +166,23 @@ export const useGameState = () => {
           startIndexUpper, startIndexLower,
           verticalPosUpper, verticalPosLower});
       } else if (gamePhase === STARTING_PHASE || gamePhase === SIMULTANEOUS_SELECT || gamePhase === SIMULTANEOUS_SELECT_UPPER) {
+=======
+  const handleSButtonPress = useCallback(async (index, updateCursorPositionUpper, holeRefs, verticalPosUpper, turnBasedSowing) => {
+    if (!isSowingUpper) {
+        // The logic that mimics the 'S' key press
+      if (gamePhase === 'TURN_BASED_SELECT' && currentTurn === PLAYER_UPPER) {
+        await updateCursorPositionUpper(holeRefs, index, verticalPosUpper);
+        setGamePhase('TURN_BASED_SOWING');
+        turnBasedSowing(index, PLAYER_UPPER);
+      } else if (gamePhase === 'STARTING_PHASE' || gamePhase === 'SIMULTANEOUS_SELECT' || gamePhase === 'SIMULTANEOUS_SELECT_UPPER') {
+>>>>>>> Factorized gameState variables.
         await updateCursorPositionUpper(holeRefs, index, verticalPosUpper);
         setStartingPositionUpper(index);
       }
     }
   },[isSowingUpper, gamePhase, currentTurn, setGamePhase, setStartingPositionUpper]);
 
+<<<<<<< HEAD
   const handleArrowDownPress = useCallback(async (index, updateCursorPositionUpper, updateCursorPositionLower, holeRefs, topHouseRef, lowHouseRef, verticalPosUpper, verticalPosLower, turnBasedSowing, setShakeCursor, handleWrongSelection ) => {
     if (!isSowingLower) {
       // The logic that mimics the 'ArrowDown' key press
@@ -166,6 +203,16 @@ export const useGameState = () => {
           startIndexUpper, startIndexLower,
           verticalPosUpper, verticalPosLower});
       } else if (gamePhase === STARTING_PHASE || gamePhase === SIMULTANEOUS_SELECT || gamePhase === SIMULTANEOUS_SELECT_LOWER) {
+=======
+  const handleArrowDownPress = useCallback(async (index, updateCursorPositionLower, holeRefs, verticalPosLower, turnBasedSowing) => {
+    if (!isSowingLower) {
+      // The logic that mimics the 'ArrowDown' key press
+      if (gamePhase === 'TURN_BASED_SELECT' && currentTurn === PLAYER_LOWER) {
+        await updateCursorPositionLower(holeRefs, index, verticalPosLower);
+        setGamePhase('TURN_BASED_SOWING');
+        turnBasedSowing(index, PLAYER_LOWER);
+      } else if (gamePhase === 'STARTING_PHASE' || gamePhase === 'SIMULTANEOUS_SELECT' || gamePhase === 'SIMULTANEOUS_SELECT_LOWER') {
+>>>>>>> Factorized gameState variables.
         await updateCursorPositionLower(holeRefs, index, verticalPosLower);
         setStartingPositionLower(index);
       }
@@ -192,4 +239,8 @@ export const useGameState = () => {
     isStartButtonPressed, setIsStartButtonPressed,
     resetGame, toggleTurn, startButtonPressed, handleSButtonPress, handleArrowDownPress,
   }
+<<<<<<< HEAD
+=======
+
+>>>>>>> Factorized gameState variables.
 };
